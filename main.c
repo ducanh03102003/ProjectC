20352336e54956cfff5bf72ac353db64049ce7ac
Maverick <86279612+ducanh03102003@users.noreply.github.com>
2023-05-01 10:01:12 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <math.h>
-#include <complex.h>
 #include "roots.h"
+
+typedef struct Complex {
+    float real;
+    float imag;
+} Complex;
 
 float compute_c1(float a1, float a2, float y0, float y1, float lambda1, float lambda2) {
     return (y1 - (lambda2 * y0)) / (lambda1 - lambda2);
@@ -22,24 +26,24 @@ int main() {
     printf("Enter the two initial conditions (y0, y1): ");
     scanf("%f %f", &y0, &y1);
 
-    double complex root1, root2;
+    Complex root1, root2;
     find_roots(a1, a2, &root1, &root2);
 
-    if (cimag(root1) == 0 && cimag(root2) == 0) {
-        if (creal(root1) == creal(root2)) { // Repeating real roots
+    if (root1.imag == 0 && root2.imag == 0) {
+        if (root1.real == root2.real) { // Repeating real roots
             float C1, C2;
             C1 = y0;
-            C2 = y1 - creal(root1) * y0;
-            printf("y(t) = (%.2f + %.2ft)e^(%.2ft)\n", C1, C2, creal(root1));
+            C2 = y1 - root1.real * y0;
+            printf("y(t) = (%.2f + %.2ft)e^(%.2ft)\n", C1, C2, root1.real);
         } else { // Distinct real roots
             float C1, C2;
-            C1 = compute_c1(a1, a2, y0, y1, creal(root1), creal(root2));
-            C2 = compute_c2(a1, a2, y0, y1, creal(root1), creal(root2));
-            printf("y(t) = %.2fe^(%.2ft) + %.2fe^(%.2ft)\n", C1, creal(root1), C2, creal(root2));
+            C1 = compute_c1(a1, a2, y0, y1, root1.real, root2.real);
+            C2 = compute_c2(a1, a2, y0, y1, root1.real, root2.real);
+            printf("y(t) = %.2fe^(%.2ft) + %.2fe^(%.2ft)\n", C1, root1.real, C2, root2.real);
         }
     } else { // Complex conjugate roots
-        float alpha = creal(root1);
-        float beta = cimag(root1);
+        float alpha = root1.real;
+        float beta = root1.imag;
         float C = sqrt(y0 * y0 + (y1 - alpha * y0) * (y1 - alpha * y0)) / beta;
         float theta = compute_theta(alpha, beta, y0, y1) * 180 / M_PI;
         printf("y(t) = %.2f e^(%0.2f t)cos(%.2f t + %.2f)\n", C, alpha, beta, theta);
